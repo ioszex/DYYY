1c10e6e95c5ab099b416add9566a99eb7e8684b0
ioszex <137703847+ioszex@users.noreply.github.com>
2025-04-19 14:49:16 +0800

    更新 DYYYSettingViewController.m

--- DYYYSettingViewController.m
+++ DYYYSettingViewController.m
@@ -490,7 +490,7 @@ typedef NS_ENUM(NSInteger, DYYYSettingItemType) {
 
 - (void)setupFooterLabel {
     self.footerLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, 50)];
-    self.footerLabel.text = @"Developer By @huamidev\nVersion: 2.2.4";
+    self.footerLabel.text = @"Developer By X\nVersion: 2.3.0";
     self.footerLabel.textAlignment = NSTextAlignmentCenter;
     self.footerLabel.font = [UIFont systemFontOfSize:14];
     self.footerLabel.textColor = [UIColor secondaryLabelColor];
