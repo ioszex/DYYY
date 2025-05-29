6b58f9c02fa649bfde9365ef38b39a4f7498efd9
ioszex <137703847+ioszex@users.noreply.github.com>
2025-05-29 10:42:58 +0800

    更新 AwemeHeaders.h

--- AwemeHeaders.h
+++ AwemeHeaders.h
@@ -377,6 +377,7 @@ typedef NS_ENUM(NSInteger, MediaType) {
 @end
 
 @interface DYYYSettingViewController : UIViewController
+@property (nonatomic, strong) NSArray<NSArray<DYYYSettingItem *> *> *settingSections;
 @end
 
 @interface AWEElementStackView : UIView
